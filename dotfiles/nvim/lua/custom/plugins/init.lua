13d28ba9a449297cba21844b8533f1660dc93e62
johnlangs <me@johnlangs.dev>
2024-07-02 22:01:36 -0500

    dev up

---
+++ dotfiles/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,30 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'chriskempson/base16-vim',
+  },
+  {
+    'typicode/bg.nvim',
+  },
+  {
+    'jacoborus/tender.vim',
+  },
+  {
+    'bluz71/vim-moonfly-colors',
+  },
+  {
+    'rebelot/kanagawa.nvim',
+  },
+  {
+    'Mofiqul/vscode.nvim',
+  },
+  {
+    'sainnhe/sonokai',
+  },
+  {
+    'sindrets/diffview.nvim',
+  },
+}
